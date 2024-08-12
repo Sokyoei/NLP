41347fd6e246fce30a786e56140328294250fddb
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2024-08-12 16:18:10 +0800

    add word embedding and attention

--- NLP/model/SelfAttention.py
+++ NLP/model/SelfAttention.py
@@ -3,10 +3,34 @@ Self-Attention 注意力机制
 """
 
 import torch
+from torch import Tensor, nn
+
+
+class SelfAttention(nn.Module):
+
+    def __init__(self, dim: int, dk: int, dv: int) -> None:
+        super().__init__()
+        self.scale = dk**-0.5
+        self.q = nn.Linear(dim, dk)
+        self.k = nn.Linear(dim, dk)
+        self.v = nn.Linear(dim, dv)
+
+    def forward(self, x):
+        q: Tensor = self.q(x)
+        k: Tensor = self.k(x)
+        v: Tensor = self.v(x)
+
+        attn: Tensor = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        out = attn @ v
+        return out
 
 
 def main():
-    pass
+    attn = SelfAttention(2, 2, 3)
+    x = torch.randn((1, 4, 2))
+    out: Tensor = attn(x)
+    print(out.shape)
 
 
 if __name__ == "__main__":
